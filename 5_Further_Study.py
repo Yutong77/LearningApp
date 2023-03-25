9aecbb2008b49451478bd820de3e7fa603c07c56
Yutong77 <118307353+Yutong77@users.noreply.github.com>
2023-03-25 14:36:11 +0100

    Add files via upload

---
+++ 5_Further_Study.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+from PIL import Image
+
+st.header("Further Study")
+st.markdown(r'''
+            🎉 You've reached the last page of this app, 
+            and we hope this app is useful for your studies on fault currents!  
+            '''
+            r'''
+            This App discusses basic methods of analyzing short-circuit currents as well as visualizes some types of short-circuit currents. 
+            If you are interested in learning more about fault currents, 
+            you can also refer to the following textbooks and articles, which are also references for this App.
+            ''')
+st.markdown("🕮 **References**")
+st.markdown(r'''
+            [1]	J. Duncan Glover, M. S. Sarma, T. J. Overbye. 
+            Power System Analysis and Design, edition 7th., Cengage Learning, Stamford, June 2022.
+            [2]	https://link.springer.com/book/10.1007/978-3-662-44547-1
+            ''')
